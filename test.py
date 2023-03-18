a031c7108af56e2007da1b98edcb210277db1fb8
wanruu <1155107790@link.cuhk.edu.hk>
2023-03-19 01:21:19 +0800

    clean code

--- test.py
+++ test.py
@@ -3,16 +3,13 @@
 import config
 from metrics import Metrics
 from data import NERDataset, gen_bio, RecordsTool
-from train import train
 from model import BertNER
 
-import time
 import torch
 import numpy as np
 from tqdm import tqdm
 from torch.utils.data import DataLoader
 from transformers import BertTokenizer
-from transformers.optimization import get_cosine_schedule_with_warmup, AdamW
 
 
 def test_single(data, model):
@@ -49,15 +46,11 @@ if __name__ == "__main__":
 
     print("* Preparing data...")
     tool = RecordsTool(config.data_file, config.data_headers, ratios=[1,1])
-    train_records, test_records = tool.splited
+    _, test_records = tool.splited
 
-    train_dataset = NERDataset(train_records, gen_bio, tokenizer)
     test_dataset = NERDataset(test_records, gen_bio, tokenizer)
-
-    train_dataloader = DataLoader(train_dataset, config.batch_size, shuffle=False, collate_fn=train_dataset.collate_fn)
     test_dataloader = DataLoader(test_dataset, 1, shuffle=False, collate_fn=test_dataset.collate_fn)
-    print(len(train_dataset), len(test_dataset), len(train_dataset)+len(test_dataset))
 
-
+    print("* Starting testing...")
     model.to(config.device)
     test(test_dataloader, model)
