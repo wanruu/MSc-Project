be4ac681699aff7d52b1675e6ca190c33901a812
wanruuu <1155107790@link.cuhk.edu.hk>
2023-06-02 13:46:43 +0800

    remove bio_11

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -1,4 +1,4 @@
 python ../src/preprocess.py \
-    --raw_data_file "../data/raw/20230412.xls" \
-    --save_dir "../data/bio_11-20230531/" \
-    --tag_method "bio_11"
+    --raw_data_file ../data/raw/20230601.xls \
+    --save_dir ../data/20230601-bio_13_split/ \
+    --tag_method bio_13_split
--- src/utils/bio_13_split.py
+++ src/utils/bio_13_split.py
@@ -1,3 +1,7 @@
+# ignore ( and ) and space
+# spilt by ( and ) and space
+import re
+
 # label
 label2id = {
     "O": 0,
@@ -10,7 +14,9 @@ label2id = {
     "I-building": 7,
     "I-unit": 8,
     "I-floor": 9,
-    "I-room": 10
+    "I-room": 10,
+    "B-floor|room": 11,
+    "I-floor|room": 12,
 }
 id2label = {label2id[label]: label for label in label2id}
 
@@ -34,46 +40,47 @@ def generate_tags(record: list):
                 new_labels[idx] = "I-" + new_labels[idx]
         return new_labels
 
-    def _match_entity(addr, entity, split=False):
+    def _match_entity(addr, entity):
         """
         params addr: raw address, e.g., 碧湖豪苑-十八栋 (18栋502室)
         params entity: e.g. 碧湖豪苑
         return: indexes, e.g. [0,1,2,3] 
         """
-        # full matching
-        if entity in addr:
-            start_idx = addr.index(entity)
-            end_idx = start_idx + len(entity)
-            return list(range(start_idx, end_idx))
+        split_entity = re.split(r"[() ]", entity)
+        
+        res_idxs = []
+        last_idx = -1
+        for part in split_entity:
+            sub_addr = addr[last_idx+1:]
+            if part not in sub_addr:
+                return None
 
-        # split matching (only for POI)
-        elif split:
-            res_idxs = []
-            last_idx = -1
-            for char in entity:
-                sub_addr = addr[last_idx+1:]
-                if char in sub_addr:
-                    last_idx = sub_addr.index(char) + last_idx + 1
-                    res_idxs.append(last_idx)
-            return res_idxs
-        return None
+            last_idx += sub_addr.index(part) + len(part)
+            res_idxs += list(range(last_idx-len(part)+1, last_idx+1))
+        return res_idxs
+
 
     # retrieve data
     addr = record[0]
     entities = record[1:6]
     labels = ["POI", "building", "unit", "floor", "room"]
-    
+
     # init result
     bio = ["O" for _ in addr]
 
     # generate
     for entity, label in zip(entities, labels):
-        match_res = _match_entity(addr, entity, split=label=="POI")
+        match_res = _match_entity(addr, entity)
         if match_res is None:
             return None
 
         for idx in match_res:
-            bio[idx] = label
+            if bio[idx] == "floor" and label == "room":
+                bio[idx] = "floor|room"
+            elif bio[idx] != "O":
+                return None
+            else:
+                bio[idx] = label
 
     # prefix: B-, I-
     bio = _add_prefix(bio)
